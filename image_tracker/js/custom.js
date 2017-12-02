3638cfa4a4350e8e89e0e83d7a9e2b0c7a0f8c9f
Dilip <dilipkumar4813@gmail.com>
2017-12-02 23:13:46 +0530

    retest

---
+++ image_tracker/js/custom.js
@@ -0,0 +1,57 @@
+var World = {
+	init: function initFn(){
+		this.imageOverlayFn();
+	},
+	createOverlays: function createOverlaysFn() {
+
+		this.targetCollectionResource = new AR.TargetCollectionResource("assets/magazine.wtc", {
+        });
+
+        this.tracker = new AR.ImageTracker(this.targetCollectionResource, {
+            onTargetsLoaded: this.worldLoaded,
+            onError: function(errorMessage) {
+            	alert(errorMessage);
+            }
+        });
+
+		var imgOne = new AR.ImageResource("assets/imageOne.png");
+		var overlayOne = new AR.ImageDrawable(imgOne, 1, {
+			translate: {
+				x:-0.15
+			}
+
+		});
+
+		var sound = new AR.Sound("assets/jellylude.mp3", {
+  		
+  		});
+
+
+		var pageOne = new AR.ImageTrackable(this.tracker, "*", {
+			drawables: {
+				cam: overlayOne
+			},
+			onImageRecognized: sound.play(),
+            onError: function(errorMessage) {
+            	alert(errorMessage);
+            }
+		});
+	},
+	removeLoadingBar: function() {
+		if (!World.loaded) {
+			var e = document.getElementById('loadingMessage');
+			e.parentElement.removeChild(e);
+			World.loaded = true;
+		}
+	},
+
+	worldLoaded: function worldLoadedFn() {
+		var cssDivLeft = " style='display: table-cell;vertical-align: middle; text-align: right; width: 50%; padding-right: 15px;'";
+		var cssDivRight = " style='display: table-cell;vertical-align: middle; text-align: left;'";
+		document.getElementById('loadingMessage').innerHTML =
+			"<div" + cssDivLeft + ">Scan Target &#35;1 (surfer):</div>" +
+			"<div" + cssDivRight + "><img src='assets/surferOne.png'></img></div>";
+	}
+};
+
+World.init();
