4d3eb282e66cf5d4d407701fa3d815c300dbcebd
Dilip <dilipkumar4813@gmail.com>
2017-12-02 22:59:56 +0530

    retest

--- image_tracker/js/custom.js
+++ image_tracker/js/custom.js
@@ -14,23 +14,38 @@ var World = {
             }
         });
 
-		var imageOne = new AR.ImageResource("assets/imageOne.jpg");
-
-		var imageDrawable1 = new AR.ImageDrawable(imageOne, 1, {
-			transalte:{
+		var imgOne = new AR.ImageResource("assets/imageOne.png");
+		var overlayOne = new AR.ImageDrawable(imgOne, 1, {
+			translate: {
 				x:-0.15
 			}
+
 		});
 
-		var pageOne = new AR.ImageTrackable(this.tracker,'*',{
+		var pageOne = new AR.ImageTrackable(this.tracker, "*", {
 			drawables: {
-				cam: imageDrawable1
+				cam: overlayOne
 			},
-			onImageRecognized: this.playSound(),
-			onError: function(errorMessage){
-				alert(errorMessage);
-			}
+			onImageRecognized: this.removeLoadingBar,
+            onError: function(errorMessage) {
+            	alert(errorMessage);
+            }
 		});
+	},
+	removeLoadingBar: function() {
+		if (!World.loaded) {
+			var e = document.getElementById('loadingMessage');
+			e.parentElement.removeChild(e);
+			World.loaded = true;
+		}
+	},
+
+	worldLoaded: function worldLoadedFn() {
+		var cssDivLeft = " style='display: table-cell;vertical-align: middle; text-align: right; width: 50%; padding-right: 15px;'";
+		var cssDivRight = " style='display: table-cell;vertical-align: middle; text-align: left;'";
+		document.getElementById('loadingMessage').innerHTML =
+			"<div" + cssDivLeft + ">Scan Target &#35;1 (surfer):</div>" +
+			"<div" + cssDivRight + "><img src='assets/surferOne.png'></img></div>";
 	},
 	playSound: function playSoundFn(){
 		var sound = new AR.Sound("assets/jellylude.mp3", {
