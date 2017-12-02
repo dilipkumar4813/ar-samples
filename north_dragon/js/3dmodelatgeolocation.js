e282b2d186d464fad54aca1ad9165767263c36c9
Dilip <dilipkumar4813@gmail.com>
2017-12-02 15:28:59 +0530

    Change of altitude

--- north_dragon/js/3dmodelatgeolocation.js
+++ north_dragon/js/3dmodelatgeolocation.js
@@ -11,7 +11,7 @@ var World = {
 		/*
 			First a location where the model should be displayed will be defined. This location will be relativ to the user.	
 		*/
-		var location = new AR.RelativeLocation(null, 5, 0, -2);
+		var location = new AR.RelativeLocation(null, 5, 0, -5);
 
 		/*
 			Next the model object is loaded.
